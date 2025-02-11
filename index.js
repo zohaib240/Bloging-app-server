f749eb056a88888c9b41df1839b69e70923b6084
zohaib240 <zohaibjs123@gmail.com>
2025-02-12 00:09:22 +0500

    index.js

--- index.js
+++ index.js
@@ -23,16 +23,16 @@ const app = express(); // `app` ko pehle initialize karo
 
 
 
-// const corsOptions = {
-//   origin: 'http://localhost:5173', // Frontend domain
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-//   credentials: true, // Allow credentials (cookies, etc.)
-// };
+const corsOptions = {
+  origin: 'http://localhost:5173', // Frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true, // Allow credentials (cookies, etc.)
+};
 
 // Use CORS middleware globally
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(cors({}))
+// app.use(cors({}))
 
 // Middleware
 app.use(express.json());
