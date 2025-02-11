14732e122d1dc1ccdb275a8310f10a2ec22b8205
zohaib240 <zohaibjs123@gmail.com>
2025-02-12 00:03:18 +0500

    index.js

--- index.js
+++ index.js
@@ -16,16 +16,23 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 dotenv.config();
 
+
+
 const app = express(); // `app` ko pehle initialize karo
 
-const corsOptions = {
-  origin: 'http://localhost:5173', // Frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  credentials: true, // Allow credentials (cookies, etc.)
-};
+
+
+
+// const corsOptions = {
+//   origin: 'http://localhost:5173', // Frontend domain
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+//   credentials: true, // Allow credentials (cookies, etc.)
+// };
 
 // Use CORS middleware globally
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors())
 
 // Middleware
 app.use(express.json());
@@ -35,7 +42,11 @@ app.use(cookieParser());
 // Routes
 app.use("/api/v1", router);
 app.use("/api/v1", blogrouter);
-
+app.get("/getcookie",(req,res)=>{
+  res.json({
+    cookie : req.cookies
+  })
+})
 
 
 // Database connection and server start
