e9ec7f556efb183d01a09c4674963470c6a998ae
zohaib240 <zohaibjs123@gmail.com>
2025-02-24 10:35:03 +0500

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config()
 import express from "express";
 import connectDB from "./src/db/index.js";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import router from './src/routes/users.routes.js'; // Correct import path
 import blogrouter from "./src/routes/blogs.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
-// dotenv.config();
+dotenv.config();
 
 const app = express(); // `app` ko pehle initialize karo
 
