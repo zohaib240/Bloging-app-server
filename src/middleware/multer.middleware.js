9742b7b9000a4fbac56b12741a147b39e77d9e43
zohaib240 <zohaibjs123@gmail.com>
2025-02-25 08:07:38 +0500

    multer

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,7 +1,18 @@
-import multer from "multer";
+// import multer from "multer";
 
-const storage = multer.memoryStorage();
+// const storage = multer.memoryStorage();
 
-export const upload = multer({
-    storage,
-});
+// export const upload = multer({
+//     storage,
+// });
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 5 * 1024 * 1024 }, // Max file size: 5MB
+    fileFilter: (req, file, cb) => {
+      if (!file.mimetype.startsWith("image/")) {
+        return cb(new Error("Only images are allowed"), false);
+      }
+      cb(null, true);
+    },
+  });
